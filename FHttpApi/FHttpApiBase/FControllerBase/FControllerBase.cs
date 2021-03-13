1fe46ce652d8e23d80212d70b26c7775811a3c20
AmirHosseinBaderan <amir0hossin0pr@gmail.com>
2021-03-14 00:34:26 +0330

    insert friends controller base

---
+++ FHttpApi/FHttpApiBase/FControllerBase/FControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using FHttpApi.ObjectResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FTeam.FHttpApiBase.FControllerBase
+{
+    public class FControllerBase : ControllerBase
+    {
+        public async Task<FOkObjectResult> FOk(FOkObjectResult objectResult) => await Task.Run(() => objectResult);
+    }
+}
