26d41c73fabe7e95282ed0aac65f8606b43f57c7
amir hossein baderan <amir0hossin0pr@gmail.com>
2021-03-13 15:02:21 +0330

    Start Project

---
+++ FHttpApi/FHttpApi/ActionResult/IFtActionResult.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FTeam.FHttpApi.ActionResult
+{
+    public interface IFtActionResult : IActionResult
+    {
+
+    }
+
+
+}
