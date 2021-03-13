9fbb7e6e3ea86bfaf21d3d0219ed276c7c3739f2
amir hossein baderan <amir0hossin0pr@gmail.com>
2021-03-13 15:50:45 +0330

    insert ok result

---
+++ FHttpApi/FHttpApi/ObjectResults/FOkObjectResult.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace FHttpApi.ObjectResults
+{
+    [DefaultStatusCode(DefaultStatusCode)]
+    public class FOkObjectResult : ObjectResult
+    {
+        //Default Status Code
+        private const int DefaultStatusCode = StatusCodes.Status200OK;
+
+        public FOkObjectResult(object value) : base(value)
+        {
+            StatusCode = DefaultStatusCode;
+        }
+    }
+}
